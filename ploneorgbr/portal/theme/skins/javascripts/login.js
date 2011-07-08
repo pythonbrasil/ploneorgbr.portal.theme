8d526c6dab81d7b514053325e9acba96e2d8b567
Davi Lima <davi@simplesconsultoria.com.br>
2011-07-08 19:01:42 -0300

    Adds login slider resources

---
+++ ploneorgbr/portal/theme/skins/javascripts/login.js
@@ -0,0 +1,24 @@
+$(function(){
+
+    var url = window.location.href;
+    $("#login-link").click(function() {
+        // the login template is static html so we define came_from
+        // here as there is no TAL available
+        $('#came-from-input').val(url);
+        $("#login-popup").slideToggle();
+        return false;
+    });
+
+    $("#login-slide-form").submit(function(){
+    // for testing purposes, we want the form action to point to
+    // /plone.org/login_form if we are local or staging
+    if(url.indexOf('http://plone.org.br') != 0 && url.indexOf('https://plone.org.br') != 0){
+        if(url.indexOf('http://staging.plone.org.br') == 0 || url.indexOf('https://staging.plone.org.br') == 0){
+            $(this).attr('action', 'https://staging.plone.org/login_form');
+        } else {
+            $(this).attr('action', '/plone.org.br/login_form');
+        }
+    }
+});
+
+});
